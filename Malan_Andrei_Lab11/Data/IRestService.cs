c34161a9ec8d9c0d82890c399c47e4a8b430b005
AndreiMalan <andreimalan10@gmail.com>
2022-12-21 15:34:41 +0200

    Add project files.

---
+++ Malan_Andrei_Lab11/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Malan_Andrei_Lab11.Models;
+
+
+namespace Malan_Andrei_Lab11.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
